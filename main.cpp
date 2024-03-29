e505810685c8ad1372adadcb3b405617b78095e7
Zhanibek Bakin <zhanibek.bakin@gmail.com>
2024-03-29 18:49:09 +0000

    Add usage function And add tasks.csv file

--- main.cpp
+++ main.cpp
@@ -7,12 +7,31 @@ void signal_exit_handler(int signum) {
     // Terminate program
     exit(signum);
 }
+static void usage()
+{
+    std::cerr << "syntax: " << "ToDoList" << " [global options] <command> [command options]" << std::endl;
+    std::cerr << std::endl;
+    std::cerr << "Global options are:" << std::endl;
+    std::cerr << "  --help (-h)      Display this help" << std::endl;
+    std::cerr << "  --verbose (-v)   Enable verbose mode" << std::endl;
+    std::cerr << "  --quiet (-q)     Disable verbose mode" << std::endl;
+    std::cerr << "  -f <file>        Specify ToDoList filename" << std::endl;
+    std::cerr << std::endl;
+    std::cerr << "Commands are:" << std::endl;
+    std::cerr << "  add              Add a Task to the list" << std::endl;
+    std::cerr << "  show             Display the tasks in the ToDoList" << std::endl;
+    std::cerr << "  mark-complete    Update the status of specified Task" << std::endl;
+    std::cerr << std::endl;
+    std::cerr << "Type 'ToDoList command --help' to see the syntax of 'command'." << std::endl;
+}
 
 int main() {
 
     signal(SIGINT, signal_exit_handler);
     ToDoList myList;
     int countInput = 2;
+
+    usage();
     // Enter tasks step
     while(countInput) {
         --countInput;
